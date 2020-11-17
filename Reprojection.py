c95ad67c6fd77e7003fa146099b625083877401a
Witold Sempruch <witsemp@gmail.com>
2020-11-17 18:43:07 +0100

    Added calculation of matrix from vector and quaternion, still doesn't work

--- Reprojection.py
+++ Reprojection.py
@@ -3,7 +3,7 @@ import open3d as o3d
 import cv2 as cv
 from lxml import etree
 import re
-import pathlib
+from scipy.spatial.transform import Rotation
 
 
 def lerp(x, x1, x2, y1, y2):
@@ -17,10 +17,11 @@ def lerp(x, x1, x2, y1, y2):
 
 
 def load_depth_image(depth_image_index: int):
-    base_path = '/home/witsemp/saved_test/chair/1a74a83fa6d24b3cacd67ce2c72c02e/depth/'
+    base_path = '/home/witsemp/saved_big/chair/1a74a83fa6d24b3cacd67ce2c72c02e/depth/'
     file_name = str(depth_image_index) + ".png"
     depth_image_path = base_path + file_name
     depth_image = cv.imread(depth_image_path, flags=cv.IMREAD_ANYDEPTH)
+    print(depth_image.shape)
     return depth_image
 
 
@@ -40,9 +41,35 @@ def get_transform_matrix(xml_file_path: str, index: int):
     transform_matrix = np.array([rows[0], rows[1], rows[2], rows[3]])
     return transform_matrix
 
+def get_vector_quaternion(xml_file_path: str, index: int):
+    rows = []
+    root = etree.parse(xml_file_path)
+    file = root.findall('File[@Index=' + '"' + str(index) + '"' + ']')
+    position = str(etree.tostring(file[0].findall('Position')[0]))
+    position = re.search(rf'<Position>(.+?)</Position>', position).group(1)
+    position = position.replace("(", '').replace(")", '').split(', ')
+    rotation = str(etree.tostring(file[0].findall('Rotation')[0]))
+    rotation = re.search(rf'<Rotation>(.+?)</Rotation>', rotation).group(1)
+    rotation = rotation.replace("(", '').replace(")", '').split(', ')
+    position = [float(element) for element in position]
+    rotation = [float(element) for element in rotation]
+    return position, rotation
 
 def inverse_transform_matrix(transform_matrix):
     return np.linalg.inv(transform_matrix)
+
+def rotation_matrix_from_quaternion(quaternion):
+    r = Rotation.from_quat(quaternion)
+    return r.as_matrix()
+
+
+def transform_matrix_vector_quaternion(vector, quaternion):
+    rotation_matrix = rotation_matrix_from_quaternion(quaternion)
+    translation_vector = np.array([vector[0], vector[1], vector[2]])
+    bottom_row = np.array([0.0, 0.0, 0.0, 1.0])
+    out = np.hstack((rotation_matrix, translation_vector.reshape(3, 1)))
+    out = np.vstack((out, bottom_row))
+    return out
 
 
 def reproject(depth, fov, condition):
@@ -51,14 +78,14 @@ def reproject(depth, fov, condition):
 
     point_cloud = list()
     h, w = np.shape(depth)
-    fov_x = fov
-    fov_y = fov * h / w
+    fov_x = fov * h / w
+    fov_y = fov
     delta = 0.5
     for i in range(h):
         for j in range(w):
             if condition(depth[i, j]):
-                v = -_normalize(i + delta, h, fov_x)
-                u = -_normalize(j + delta, w, fov_y)
+                v = -_normalize(i + delta, h, fov_y)
+                u = -_normalize(j + delta, w, fov_x)
                 pt = np.array([u, v, 1])
                 pt = pt * depth[i, j]
                 point_cloud.append(pt)
@@ -76,31 +103,49 @@ def pcl_from_images(depth_image, camera_matrix):
     return pcl
 
 
-def reproject_with_camera_matrix(index1, index2, xml_file_path, camera_matrix):
+def reproject_with_camera_matrix(index1, index2, xml_file_path, camera_matrix, visualise_images=False):
     div = 10000.0
     depth_image1 = load_depth_image(index1)
     depth_image2 = load_depth_image(index2)
     print("Type:\n", depth_image1.dtype)
     print("---------")
-    print("Image values:\n", depth_image1)
+    print("Shape:\n", depth_image1.shape)
     print("---------")
-    depth_image1 = depth_image1 / div
-    depth_image2 = depth_image2 / div
-    print("Scaled to metric:\n", depth_image1)
+    print("Image 1 values:\n", depth_image1)
+    print("---------")
+    print("Image 2 values:\n", depth_image2)
+    print("---------")
+    depth_image1_metric = depth_image1 / div
+    depth_image2_metric = depth_image2 / div
+    print("Scaled to metric:\n", depth_image1_metric)
+    print("---------")
+    print("Scaled to metric:\n", depth_image2_metric)
     transform_matrix1 = get_transform_matrix(xml_file_path, index1)
     transform_matrix2 = get_transform_matrix(xml_file_path, index2)
     inverse_matrix1 = inverse_transform_matrix(transform_matrix1)
     inverse_matrix2 = inverse_transform_matrix(transform_matrix2)
-    pcl1 = pcl_from_images(depth_image1, camera_matrix)
-    pcl2 = pcl_from_images(depth_image2, camera_matrix)
+    pos1, rot1 = get_vector_quaternion(xml_file_path, index1)
+    pos2, rot2 = get_vector_quaternion(xml_file_path, index2)
+    transform_matrix1_vq = transform_matrix_vector_quaternion(pos1, rot1)
+    transform_matrix2_vq = transform_matrix_vector_quaternion(pos2, rot2)
+    inverse_matrix1 = inverse_transform_matrix(transform_matrix1)
+    inverse_matrix2 = inverse_transform_matrix(transform_matrix2)
+    inverse_matrix1_vq = inverse_transform_matrix(transform_matrix1_vq)
+    inverse_matrix2_vq = inverse_transform_matrix(transform_matrix2_vq)
+    pcl1 = pcl_from_images(depth_image1_metric, camera_matrix)
+    pcl2 = pcl_from_images(depth_image2_metric, camera_matrix)
     point_cloud1 = o3d.PointCloud()
     point_cloud2 = o3d.PointCloud()
     point_cloud1.points = o3d.Vector3dVector(pcl1)
     point_cloud2.points = o3d.Vector3dVector(pcl2)
     point_cloud1.transform(transform_matrix1)
-    point_cloud2.transform(transform_matrix1)
-    # point_cloud2.transform(np.dot(transform_matrix1, inverse_matrix2))
+    point_cloud2.transform(transform_matrix2)
+    # point_cloud2.transform(np.dot(inverse_matrix2, transform_matrix1))
     o3d.visualization.draw_geometries([point_cloud1, point_cloud2])
+    if visualise_images:
+        cv.imshow('depth1', depth_image1)
+        cv.imshow('depth2', depth_image2)
+        cv.waitKey(0)
 
 
 def reproject_with_fov(index1, index2, xml_file_path, fov, near, far, visualise_images=False):
@@ -111,13 +156,15 @@ def reproject_with_fov(index1, index2, xml_file_path, fov, near, far, visualise_
     print("---------")
     print("Image values:\n", depth_image1)
     print("---------")
+    print("Image 2 values:\n", depth_image2)
+    print("---------")
     depth_image1_metric = depth_image1 / div
     depth_image2_metric = depth_image2 / div
-    print("Scaled to metric:\n", depth_image1)
+    print("Scaled to metric 1:\n", depth_image1_metric)
+    print("---------")
+    print("Scaled to metric 2:\n", depth_image2_metric)
     transform_matrix1 = get_transform_matrix(xml_file_path, index1)
     transform_matrix2 = get_transform_matrix(xml_file_path, index2)
-    inverse_matrix1 = inverse_transform_matrix(transform_matrix1)
-    inverse_matrix2 = inverse_transform_matrix(transform_matrix2)
     pcl1 = reproject(depth_image1_metric, fov, lambda x: near <= x <= 0.99 * far)
     pcl2 = reproject(depth_image2_metric, fov, lambda x: near <= x <= 0.99 * far)
     point_cloud1 = o3d.PointCloud()
@@ -126,7 +173,7 @@ def reproject_with_fov(index1, index2, xml_file_path, fov, near, far, visualise_
     point_cloud2.points = o3d.Vector3dVector(pcl2)
     point_cloud1.transform(transform_matrix1)
     point_cloud2.transform(transform_matrix2)
-    # point_cloud2.transform(np.dot(transform_matrix1, inverse_matrix2))
+    # point_cloud2.transform(np.dot(inverse_matrix2, transform_matrix1))
     o3d.visualization.draw_geometries([point_cloud1, point_cloud2])
     if visualise_images:
         cv.imshow('depth1', depth_image1)
@@ -136,22 +183,27 @@ def reproject_with_fov(index1, index2, xml_file_path, fov, near, far, visualise_
 
 if __name__ == '__main__':
     xml_file_path = '/home/witsemp/work/UnityObjectRenderer/output.xml'
-    f_depth_x = 1.0580353759137747e+03
-    f_depth_y = 1.0604471766882732e+03
-    c_depth_x = 9.5177505245974123e+02
-    c_depth_y = 5.4839653660364445e+02
-    fov = np.deg2rad(53.97213)
+    # f_depth_x = 1.0580353759137747e+03
+    # f_depth_y = 1.0604471766882732e+03
+    # c_depth_x = 9.5177505245974123e+02
+    # c_depth_y = 5.4839653660364445e+02
+    f_depth_x = 1062.3
+    f_depth_y = 1062.3
+    c_depth_x = 639.5
+    c_depth_y = 479.5
+    fov = np.deg2rad(48.63164)
     near = 0.5
     far = 3.0
-    camera_matrix = np.array(
-        [1 / f_depth_x, 0., -c_depth_x / f_depth_x,
-         0., 1 / f_depth_y, -c_depth_y / f_depth_y,
-         0., 0., 1.]).reshape(3, 3)
+    camera_matrix = np.array([[1 / f_depth_x, 0., -c_depth_x / f_depth_x],
+                              [0., 1 / f_depth_y, -c_depth_y / f_depth_y],
+                              [0., 0., 1.]])
+
     camera_matrix_opencv = np.array(
-        [f_depth_x, 0., c_depth_x,
-         0., f_depth_y, c_depth_y,
-         0., 0., 1.]).reshape(3, 3)
+        [[f_depth_x, 0., c_depth_x],
+         [0., f_depth_y, c_depth_y],
+         [0., 0., 1.]])
+    # 2, 1500 - ok, 1990 - nie ok
     index1 = 2
-    index2 = 3
-    reproject_with_fov(index1, index2, xml_file_path, fov, near, far, visualise_images=True)
-    # reproject_with_camera_matrix(index1, index2, xml_file_path, camera_matrix)
+    index2 = index1 + 1
+    # reproject_with_fov(index1, index2, xml_file_path, fov, near, far, visualise_images=True)
+    reproject_with_camera_matrix(index1, index2, xml_file_path, camera_matrix, visualise_images=False)
